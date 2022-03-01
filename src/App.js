1ccf92426923224da8adb0e5501a54a682d630e5
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-03-01 18:14:11 +0100

    Lab Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
+// src/App.js
+import "./App.css";
+import { useState } from 'react';
 
 function App() {
+
+  const [fiveContacts, setContacts] = useState(contacts.slice(2, 7))
+
+  function getRandom(){
+    let randomContact = contacts[Math.floor(Math.random()*contacts.length)];
+
+    if(fiveContacts.some((elem)=> elem.id === randomContact.id)) {
+      getRandom()
+    } else {
+      setContacts(contacts=>[...contacts, randomContact])
+    }
+  }
+
+  function sortByPop(){
+    const sortedCelebs = fiveContacts.slice().sort((el1, el2)=>{return el1.popularity - el2.popularity})
+    setContacts(sortedCelebs)
+  }
+
+  function sortByName(){
+    const sortedByName = fiveContacts.slice().sort((el1, el2)=>{
+      if(el1.name < el2.name) return -1;
+      if(el2.name > el1.name) return 1;
+      return 0;
+    })
+    setContacts(sortedByName)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div>
+      <h1>IRON CONTACTS</h1>
+      <button onClick={getRandom}>Add Random Contact</button>
+      <button onClick={sortByPop}>Sort By Popularity</button>
+      <button onClick={sortByName}>Sort By Name</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+        {fiveContacts.map((contact)=>{
+            return (
+          <tr key={contact.id}>
+            <td><img src={contact.pictureUrl} alt="actor" className='tablePic'></img></td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+            <td>{contact.wonOscar ? <p>🏆</p> : <p></p>}</td>
+            <td>{contact.wonEmmy ? <p>🏆</p> : <p></p>}</td>
+          </tr>
+          )
+        })
+        }
+      </table>
     </div>
+  </div>
   );
 }
-
 export default App;
